9ea50c71841e711b013056192463a892609e156c
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-06-05 12:33:17 +0200

    add navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
-
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
 
     return (
-        <nav>
-
+        <nav className="navbar">
+            <div className="navbar-github"></div>
+            <ul className="navbar-links">
+                <NavLink to="/cds">CDs</NavLink>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/vinyls">CDs</NavLink>
+            </ul>
+            <div className="navbar-cart"></div>
         </nav>
     )
 }
