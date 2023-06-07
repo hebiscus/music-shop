44ccf57857fcf71e29d3e9fff46c9a7b3181e254
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-06-07 19:47:33 +0200

    add additional-infor for Home, external links and clear default styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@
  import klark from "../resources/product-pictures/Klark Kent.png";
  import jah from "../resources/product-pictures/jah wurzel.png";
  import backgroundVideo from "../resources/drumsHD.mp4"
+ import githubLogo from "../resources/logos/github-mark-white.svg";
+ import linkedinLogo from "../resources/logos/In-White-72.png";
 
  function Home() {
     return (
@@ -12,6 +14,18 @@
             </video>
             <SpecialOffers pictures={VinylPictures} />
             <div className="additional-info">
+                <p>For all the music lovers since 1974
+                    Isn't quite what you're looking for?
+                    Check us out on other platforms:
+                </p>
+                <a href="https://github.com/hebiscus">
+                    <img src={githubLogo} alt="Github Logo"></img>
+                    Github
+                </a>
+                <a href="https://www.linkedin.com/in/katarzyna-gasiorek/">
+                    <img src={linkedinLogo} alt="Linkedin Logo"></img>
+                    Linkedin
+                </a>
             </div>
         </>
     )
