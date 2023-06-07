69f7ef7f8e57d880e2eec5207d839b4b0839cbd9
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-06-07 17:46:55 +0200

    move background video to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,15 @@
  import xtc from "../resources/product-pictures/XTC.png";
  import klark from "../resources/product-pictures/Klark Kent.png";
  import jah from "../resources/product-pictures/jah wurzel.png";
+ import backgroundVideo from "../resources/drumsHD.mp4"
 
  function Home() {
     return (
         <>
-            <div className="special-offers">
-                <SpecialOffers pictures={VinylPictures} />
-            </div>
+            <video id="background-video" autoPlay loop muted>
+                <source src={backgroundVideo} type="video/mp4"/>
+            </video>
+            <SpecialOffers pictures={VinylPictures} />
             <div className="additional-info">
             </div>
         </>
