f7f8f33edafa0f4f33c4dab82b71478d767e591f
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-06-07 15:32:41 +0200

    add first render click previous slide tests for special offer section

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,6 +4,8 @@ import userEvent from '@testing-library/user-event';
 import {BrowserRouter, MemoryRouter} from 'react-router-dom';
 import React from 'react'
 import App from '../App';
+import SpecialOffers from '../components/SpecialOffers';
+
 
 test('renders navbar no matter the subpage', () => {
   render(<App />, {wrapper: MemoryRouter});
@@ -15,3 +17,31 @@ test('renders navbar no matter the subpage', () => {
   expect(homeLink).toBeInTheDocument();
   expect(vinylLink).toBeInTheDocument();
 });
+
+
+
+describe('special offer rendering', () => {
+  const testPictures = [
+    "xtc.png", "klark.png", "jah.png"
+  ]
+
+  test('first render shows XTC picture and data', () => {
+    render(<SpecialOffers pictures={testPictures}/>);
+  
+    const testImage = screen.getByRole('img');
+    expect(testImage).toHaveAttribute('src', 'xtc.png');
+    expect(testImage.alt).toContain("xtc");
+  })
+
+  test('click on previous slide displays jah picture and data', async () => {
+    const user = userEvent.setup();
+    render(<SpecialOffers pictures={testPictures}/>);
+
+    // const leftArrow = screen.getByRole('button', { pressed: true });
+
+    await user.click(screen.getByRole('button', { name: "previous slide" }));
+    const testImage = screen.getByRole('img', { name: "jah.png"});
+    console.log(testImage)
+    expect(testImage).toBeInTheDocument();
+  })
+})
