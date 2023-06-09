9f7d73358734dca05ee029388aa07bd476c45dc1
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-06-09 18:36:05 +0200

    add products component and test product data

---
+++ src/components/Products.js
@@ -0,0 +1,25 @@
+
+
+function Products(products) {
+
+
+
+    return(
+        <div className="">
+            <div className="genres-container">
+                <h2>Genres</h2>
+                <button>new wave</button>
+                <button>math rock</button>
+                <button>art pop</button>
+                <button>electroclash</button>
+                <button>synthpop</button>
+                <button>ambient</button>
+            </div>
+            <div className="products-container">
+
+            </div>
+        </div>
+    )
+}
+
+export default Products;
---
+++ src/components/productsData.js
@@ -0,0 +1,17 @@
+
+
+const cdData = [
+    {title: "Colin Stetson - When we were that wept for the sea",
+     cover: "/blueCD.png",
+     description: "ababa cnsdkc yes!",
+     price: "$30.00"},
+     
+     {title: "Colin Stetson - When we were that wept for the sea",
+     cover: "/blueCD.png",
+     description: "ababa cnsdkc yes!",
+     price: "$30.00"}
+]
+
+const vinylData = [{}]
+
+export { cdData, vinylData };
