af984845ebb05652d7292841091095085dcde54a
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-06-11 19:21:42 +0200

    single product page render, basic styling

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,35 @@
-import { useParams } from "react-router-dom";
+import "../styles/Product.scss";
+import { useParams, useNavigate } from "react-router-dom";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Product({ products }) {
     const { productTitle } = useParams();
-    const productData = products.filter(product => product.title === productTitle)
+    const productData = products.filter(product => product.title === productTitle)[0];
+    console.log(productData)
+    const navigate = useNavigate();
 
     return (
-        <>
-        {productTitle}
-        </>
+        <div className="product-render">
+            <div id="background-color"></div>
+            <button onClick={() => navigate(-1)}>Go Back</button>
+            <div className="product-container">
+                <img src={productData.cover} alt={productData.productTitle}></img>
+                <h3>{productTitle}</h3>
+                <p>{productData.description}</p>
+                <div className="buy-container">
+                    <p>Price: {productData.price}</p>
+                    <button className="buy-btn">
+                        Buy now
+                        <FontAwesomeIcon icon={faCartShopping} style={{color: "#ffffff",}} />
+                    </button>
+                </div>
+            </div>
+            <div className="shipping-details">
+                <h5>Shipping Details:</h5>
+                <p>Usually ships in 1 business days. This may be affected by holidays, promotions, local weather, etc.</p>
+            </div>
+        </div>
     )
 }
 
