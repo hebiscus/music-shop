5ff0be45cc0eb3a489889e5394d7cc9125659478
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-06-13 13:33:48 +0200

    change product priceto only numbers

--- src/components/productsData.js
+++ src/components/productsData.js
@@ -5,81 +5,81 @@ const cdData = [
      coverSmall: "/cd-covers/blueCD.png",
      coverBig: "/cd-covers/blueCD-big.png",
      description: "ababa cnsdkc yes!",
-     price: "$30.00"},
+     price: "30.00"},
 
      {title: "Clipping - There existed an addiction to blood",
      coverSmall: "/cd-covers/redCD.png",
      coverBig: "/cd-covers/blueCD.png",
      description: "ababa cnsdkc yes!",
-     price: "$26.90"},
+     price: "26.90"},
 
      {title: "Susumu Hirasawa - BERSERK Original Sountrack",
      coverSmall: "/cd-covers/redwhiteCD.png",
      coverBig: "/cd-covers/redwhiteCD-big.png",
      description: "ababa cnsdkc yes!",
-     price: "$50.00"},
+     price: "50.00"},
 
      {title: "Harold Budd Brian Eno - Ambient 2: The Plateaux of Mirror",
      coverSmall: "/cd-covers/desertCD.png",
      coverBig: "/cd-covers/desertCD-big.png",
      description: "ababa cnsdkc yes!",
-     price: "$3.00"},
+     price: "3.00"},
 
      {title: "JPEGMAFIA - Veteran",
      coverSmall: "/cd-covers/darkCD.png",
      coverBig: "/cd-covers/darkCD-big.png",
      description: "ababa cnsdkc yes!",
-     price: "$13.00"},
+     price: "13.00"},
 
      {title: "New Order - Power Corruption and Lies",
      coverSmall: "/cd-covers/whiteCD.png",
      coverBig: "/cd-covers/blueCD.png",
      description: "ababa cnsdkc yes!",
-     price: "$10.00"},
+     price: "10.00"},
 
      {title: "Saint Etienne - Foxbase Alpha",
      coverSmall: "/cd-covers/alienCD.png",
      coverBig: "/cd-covers/alienCD-big.png",
      description: "ababa cnsdkc yes!",
-     price: "$18.00"},
+     price: "18.00"},
 
      {title: "Björk - Verspertine",
      coverSmall: "/cd-covers/stracciatella.png",
      coverBig: "/cd-covers/stracciatella-big.png",
      description: "ababa cnsdkc yes!",
-     price: "$300.00"}
+     price: "300.00"}
 ]
 
 const vinylData = [
     {title: "XTC - Life Begins At The Hop",
     coverSmall: "/vinyl-covers/XTC.png",
     description: "ababa cnsdkc yes!",
-    price: "$300.00"},
+    price: "300.00"},
 
     {title: "Klark Kent - Don't Care",
      coverSmall: "/vinyl-covers/Klark Kent.png",
      description: "ababa cnsdkc yes!",
-     price: "$300.00"},
+     price: "300.00"},
 
      {title: "Snips - What's Your Number",
      coverSmall: "/vinyl-covers/Snips.png",
      description: "ababa cnsdkc yes!",
-     price: "$300.00"},
+     price: "300.00"},
 
      {title: "Jah Wurzel - Wuthering Heights",
      coverSmall: "/vinyl-covers/jah wurzel.png",
      description: "ababa cnsdkc yes!",
-     price: "$300.00"},
+     price: "300.00"},
 
      {title: "Yellow Banana - Verspertine",
      coverSmall: "/vinyl-covers/blankV.png",
      description: "ababa cnsdkc yes!",
-     price: "$300.00"},
+     price: "300.00"},
 
      {title: "Summon - My Mind is Blank",
      coverSmall: "/vinyl-covers/blankV.png",
      description: "ababa cnsdkc yes!",
-     price: "$300.00"}
+     price: "300.00"}
 ]
 
 export { cdData, vinylData };
